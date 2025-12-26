1257ca23d70d32700cae46b0cc94ac473cb86631
RoshanKumar9421 <yadavroshankumar8271@gmail.com>
2025-12-26 19:40:57 +0530

    deployment added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,31 @@
 import express from "express"
 import dotenv from "dotenv";
+import path from "path"
+
 import {ENV} from "./lib/env.js"
 
 dotenv.config();
 
 const app=express()
 
-console.log(ENV.PORT)
-console.log(ENV.DB_URL)
+const __dirname=path.resolve()
 
 app.get("/", (req, res)=>{
     res.status(200).json({msg:"sucess from api "})
 });
+
+app.get("/books", (req, res)=>{
+    res.status(200).json({msg:"this is book endpoin"})
+});
+
+
+//make our app ready for deployment
+if(ENV.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("/{*any}", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(ENV.PORT, ()=>console.log("server is running on port:", ENV.PORT))
