9569b37f84cea4c72d858fab7717842c8f732f05
RoshanKumar9421 <yadavroshankumar8271@gmail.com>
2025-12-27 12:25:51 +0530

    authentaction completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express"
 import dotenv from "dotenv";
-import path from "path"
+import path from "path";
+import cors from "cors";
+import {serve} from "inngest/express";
 
 import {ENV} from "./lib/env.js"
 import { connectDB } from "./lib/db.js";
+import { inngest,functions } from "./lib/inngest.js";
 
 dotenv.config();
 console.log("DB_URL loaded:", process.env.DB_URL ? "YES" : "NO");
@@ -12,6 +15,14 @@ console.log("DB_URL loaded:", process.env.DB_URL ? "YES" : "NO");
 const app=express()
 
 const __dirname=path.resolve()
+
+//middleware
+app.use(express.json())
+//credential:true meaning??=>server allows a browser to include cookies on request
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest", serve({client:inngest, functions}))
+
 
 app.get("/", (req, res)=>{
     res.status(200).json({msg:"sucess from api "})
